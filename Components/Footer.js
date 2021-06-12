47272c5f7bcedf7c52cbc5ef34d88126a7297566
nababrata <nababratadeb5@gmail.com>
2021-06-11 12:51:39 -1200

    bug fix2

--- Components/Footer.js
+++ Components/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
             </div>
           
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                <path fill="#F3F4F6" fill-opacity="1" d="M0,256L1440,32L1440,320L0,320Z">
+                <path fill="#F3F4F6" fillOpacity="1" d="M0,256L1440,32L1440,320L0,320Z">
                 </path></svg>
 
              <div className="bg-gray-100 h-full w-full">
--- Components/Team.js
+++ Components/Team.js
@@ -28,7 +28,8 @@ function Team() {
                       </p>
 
                       <button
-                       className="bg-gradient-to-br from-yellow-400 to-red-600
+                       className="bg-gradient-to-br from-yellow-400
+                        to-red-600 hover:text-white
                        px-4 py-2 mt-4 rounded-full focus:outline-none text-lg 
                        text-gray-300
                       ">Meet with our them</button>
